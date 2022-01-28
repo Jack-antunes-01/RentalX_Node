b79eada6570ded4b6acc155d2b5b8ff65c35af3a
Jack <Jackson.contato24@hotmail.com>
2022-01-28 15:32:23 -0300

    Organize repositories

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,5 +1,5 @@
-import { Category } from "../model/Category";
-import { ICategoriesRepository } from "./ICategoriesRepository";
+import { Category } from "../../model/Category";
+import { ICategoriesRepository } from "../ICategoriesRepository";
 
 interface ICreateCategoryDTO {
   name: string;
