fd644d9a7f78f424f7a4029f228e16bb4d66c848
Jack <Jackson.contato24@hotmail.com>
2022-02-01 13:43:50 -0300

    Centralize imports routs

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,10 @@
 import express from "express";
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationsRoutes } from "./routes/specifications.routes";
+import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationsRoutes);
 
-app.get("/", (req, res) => {
-  return res.json({ message: "hello world" });
-});
+app.use(router);
 
 app.listen(3000, () => console.log("Server running on port 3000"));
