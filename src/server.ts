0612124c8ef44c8e03ab346df19aa7e57ae0b7b8
Jack <Jackson.contato24@hotmail.com>
2022-02-04 17:49:19 -0300

    Add postgres on docker

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import { router } from "./routes";
 import swaggerUi from "swagger-ui-express";
 import swaggerFile from "./swagger.json";
 
+import "./database";
+
 const app = express();
 
 app.use(express.json());
